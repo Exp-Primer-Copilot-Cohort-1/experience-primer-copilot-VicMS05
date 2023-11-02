a0baca36bb589f9c1f2d42f854a2b5df626c891c
Víctor Martínez Santiago <145970377+VicMS05@users.noreply.github.com>
2023-11-02 23:16:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,74 @@
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:8080
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost'); // 8080 is the port number, localhost is the hostname
+console.log('Server running at http://localhost:8080');
+
+// Run: node comments.js
+// Access: http://localhost:8080
+
+// Path: comments.js
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:8080
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost'); // 8080 is the port number, localhost is the hostname
+console.log('Server running at http://localhost:8080');
+
+// Run: node comments.js
+// Access: http://localhost:8080
+
+// Path: comments.js
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:8080
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost'); // 8080 is the port number, localhost is the hostname
+console.log('Server running at http://localhost:8080');
+
+// Run: node comments.js
+// Access: http://localhost:8080
+
+// Path: comments.js
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:8080
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+});
+
+server.listen(8080, 'localhost'); // 8080 is the port number, localhost is the hostname
+console.log('Server running at http://localhost:8080');
+
+// Run: node comments.js
+// Access: http://localhost:8080
+
+// Path: comments.js
+// Create web server
+// Run: node comments.js
